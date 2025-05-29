6e577df0461a9c5384ff95cc185a3fa44a3a7c0c
Michael Hume <michael.a.hume@pm.me>
2025-05-28 23:37:15 -0400

    feat(tests): unit tests
    - adds test using Crate assert_cmd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_cmd::Command;
+
+    #[test]
+    fn test_arg_exists() {
+        let mut cmd = Command::cargo_bin("work").unwrap();
+        let assert = cmd.args(["-n", "Mike"]).assert();
+
+        assert.success().code(0).stdout("Hello Mike!\n");
+    }
 }
